842344f2fa522156cc55b1814b5a01c23dd743f3
tzelleke <theodros.zelleke@gmail.com>
2023-05-08 17:46:03 +0200

    chore: fixes numerous linting issues

--- app/api/auth.py
+++ app/api/auth.py
@@ -4,8 +4,8 @@ from starlette.exceptions import HTTPException
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from app.core.config import (
-    API_KEY_NAME,
     API_KEY,
+    API_KEY_NAME,
 )
 
 api_key_header_auth = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
@@ -14,5 +14,6 @@ api_key_header_auth = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 async def authenticated(api_key_header: str = Security(api_key_header_auth)):
     if api_key_header != API_KEY:
         raise HTTPException(
-            status_code=HTTP_401_UNAUTHORIZED, detail="Invalid API Key",
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key",
         )
--- app/api/v1/address.py
+++ app/api/v1/address.py
@@ -4,14 +4,11 @@ from fastapi import (
     APIRouter,
     Depends,
 )
-from starlette.status import (
-    HTTP_200_OK,
-    # HTTP_404_NOT_FOUND,
-)
 from sqlmodel import (
-    select,
     Session,
+    select,
 )
+from starlette.status import HTTP_200_OK
 
 from app.db import get_session
 from app.models import Address
@@ -20,10 +17,14 @@ router = APIRouter()
 
 
 @router.get(
-    "/", response_model=List[Address], status_code=HTTP_200_OK, tags=["Addresses"],
+    "/",
+    response_model=List[Address],
+    status_code=HTTP_200_OK,
+    tags=["Addresses"],
 )
 async def list_addresses(
-    *, session: Session = Depends(get_session),
+    *,
+    session: Session = Depends(get_session),
 ):
     addresses = session.exec(select(Address)).all()
 
